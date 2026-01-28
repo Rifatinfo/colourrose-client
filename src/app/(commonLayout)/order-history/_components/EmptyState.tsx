fffe4d90f3519923dcb0b17f61191c8a181045a6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-01-28 20:08:09 +0600

    add order-history

---
+++ src/app/(commonLayout)/order-history/_components/EmptyState.tsx
@@ -0,0 +1,9 @@
+const EmptyState = () => {
+  return (
+    <div className="rounded-lg border border-dashed p-10 text-center text-muted-foreground">
+      No orders found.
+    </div>
+  );
+}
+
+export default EmptyState;
---
+++ src/app/(commonLayout)/order-history/page.tsx
@@ -0,0 +1,11 @@
+import OrderHistory from "./_components/OrderCard";
+
+const OrderHistoryPage = () => {
+    return (
+        <div>
+            <OrderHistory />    
+        </div>
+    );
+};
+
+export default OrderHistoryPage;
